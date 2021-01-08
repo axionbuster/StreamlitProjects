c28aa7a9c2760ee3fa578c81a7ab3eb4a64f805c
YuJin Kim <axionbuster@gmail.com>
2021-01-08 00:32:59 -0800

    Play around with stocks code!

--- 01-stocks/myapp.py
+++ 01-stocks/myapp.py
@@ -2,18 +2,26 @@ import yfinance as yf
 import streamlit as st
 import pandas as pd
 
+# Markdown
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Shown are the stock closing price and volume of Apple!
 
 """)
 
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'AAPL'
 tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 
 # Open, High, Low Close, Volume, Dividends, Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
